738de886d7700422828478cf4112f4ce8683120f
DarknSolid <hampuswadum@hotmail.com>
2022-02-24 14:47:50 +0100

    Update Program.cs

--- TestSuite/Program.cs
+++ TestSuite/Program.cs
@@ -8,7 +8,7 @@ using TestSuite;
 using WebApp.Entities;
 
 
-var connectionString = "Host=localhost;Port=5555;Database=postgres;Username=postgres;Password=0206hampus;SslMode=Disable";
+var connectionString = "Host=localhost;Port=6666;Database=postgres;Username=postgres;Password=0206hampus;SslMode=Disable";
 var services = new ServiceCollection();
 services.AddDbContext<IApplicationDbContext,ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 services.AddScoped<IDogParkRepository, DogParkRepository>();
@@ -22,12 +22,12 @@ var lowerRight = new Point(new Coordinate(2, 1));
 
 var result = await repo.GetInAreaAsync(upperLeft, lowerRight);
 
-//for (int i = 0; i < 1000; i++)
-//{
-//    var park = Util.GenerateDogPark();
-//    await repo.Create(park);
-//    Console.WriteLine($"{i}");
-//}
+for (int i = 0; i < 10000; i++)
+{
+    var park = Util.GenerateDogPark();
+    await repo.Create(park);
+    Console.WriteLine($"{i}");
+}
 
 Console.WriteLine("done");
 
