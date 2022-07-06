57312b2a61ed0e6d3b92806e14dddeb3242a8c97
DarknSolid <hampuswadum@hotmail.com>
2022-07-06 11:26:28 +0200

    added browser history go back

---
+++ MobileBlazor/Extensions/NavigationManagerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileBlazor.Extensions
+{
+    public static class NavigationManagerExtensions
+    {
+        public static void GoBack(this NavigationManager navigationManager, IJSRuntime jsRuntime)
+        {
+            jsRuntime.InvokeVoidAsync("history.back");
+        }
+    }
+}
