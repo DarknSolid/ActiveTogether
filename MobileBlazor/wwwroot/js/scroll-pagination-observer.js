b38bdb711e787d21d9e681c25c422e02e9d1f2f0
DarknSolid <hampuswadum@hotmail.com>
2022-07-13 11:25:36 +0200

    fixed oberser to handle multiple observables

--- MobileBlazor/wwwroot/js/scroll-pagination-observer.js
+++ MobileBlazor/wwwroot/js/scroll-pagination-observer.js
@@ -4,19 +4,37 @@
 let previousEntryTimestamp = null;
 window.Observer = {
     observer: null,
-    Initialize: function (component, oberserverTargetId) {
-        this.observer = new IntersectionObserver(e => {
-            const entry = e[0];
-            // a cheacky and bad way to check if the time between this
-            // and last event is greater than 500 milliseconds to avoid double events:
-            if (previousEntryTimestamp === null || entry.time > previousEntryTimestamp + 500) {
-                previousEntryTimestamp = entry.time;
-                component.invokeMethodAsync('OnIntersection');
-            }
-        });
+    components: new Map(),
 
-        let element = document.getElementById(oberserverTargetId);
+    Add: function (component, observerTargetId) {
+
+        if (this.observer == null) {
+            //console.log("Observer was initialized");
+            this.observer = new IntersectionObserver(entries => {
+
+                // console.log("Intersection(s) happened: ");
+
+                entries.forEach(entry => {
+
+                    //console.log("entry: ", entry.target.id)
+
+                    const component = this.components.get(entry.target.id);
+                    // a cheacky and bad way to check if the time between this
+                    // and last event is greater than 500 milliseconds to avoid double events:
+                    if (previousEntryTimestamp === null || entry.time > previousEntryTimestamp + 500) {
+                        previousEntryTimestamp = entry.time;
+                        component.invokeMethodAsync('OnIntersection');
+                    }
+                })
+            });
+        }
+
+        let element = document.getElementById(observerTargetId);
         if (element == null) throw new Error("Target was not found");
+
+        this.components.set(observerTargetId, component);
+        //console.log("New component set:")
+        //console.log("observerTargetId: ", observerTargetId)
         this.observer.observe(element);
     }
 }
